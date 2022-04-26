59eaa0700bf69d091e479cb9c6508d7f26df0698
andynl <andy.natalino@gmail.com>
2022-04-26 23:43:44 +0700

    Add func toJSON products

--- handlers/products.go
+++ handlers/products.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 
@@ -18,10 +17,8 @@ func NewProduct(l *log.Logger) *Product {
 
 func (p *Product) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	lp := data.GetProducts()
-	data, err := json.Marshal(lp)
+	err := lp.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
 	}
-
-	rw.Write(data)
 }
