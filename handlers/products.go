9eb356e85d0825887732945a9eac73b72a285fa2
Andy Natalino <andy.natalino@gmail.com>
2022-04-26 16:53:04 +0700

    products list

---
+++ handlers/products.go
@@ -0,0 +1,27 @@
+package handlers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/andynl/go-microservices/data"
+)
+
+type Product struct {
+	l *log.Logger
+}
+
+func NewProduct(l *log.Logger) *Product {
+	return &Product{l}
+}
+
+func (p *Product) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	lp := data.GetProducts()
+	data, err := json.Marshal(lp)
+	if err != nil {
+		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+	}
+
+	rw.Write(data)
+}
