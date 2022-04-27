e0d8561a4991ec1e9d7d553f0fe4742b04df29aa
Andy Natalino <andy.natalino@gmail.com>
2022-04-27 10:12:45 +0700

    Fix products handlers

--- handlers/products.go
+++ handlers/products.go
@@ -16,6 +16,15 @@ func NewProduct(l *log.Logger) *Product {
 }
 
 func (p *Product) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		p.getProducts(rw, r)
+		return
+	}
+
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func (p *Product) getProducts(rw http.ResponseWriter, r *http.Request) {
 	lp := data.GetProducts()
 	err := lp.ToJSON(rw)
 	if err != nil {
